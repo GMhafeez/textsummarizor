3b999a96dcce583fdcfe7b873452c9dbef8da62c
unknown <gmhafeez17@gmail.com>
2024-10-09 22:33:12 +0500

    see you tomorrow

--- src/Pipeline/pipeline.py
+++ src/Pipeline/pipeline.py
@@ -7,7 +7,11 @@ load_dotenv()
 model_name="meta-llama/Llama-3.2-1B"
 access_token = os.getenv("HUGGINGFACEHUB_API_TOKEN")
 access_token_HF_TOKEN = os.getenv("HF_TOKEN")
-summarizer = pipeline("text-generation",model=model_name,torch_dtype=torch.bfloat16)
+summarizer = pipeline(
+    "text-generation",
+    model=model_name,
+    torch_dtype=torch.bfloat16
+    )
 result = summarizer("hello my name is ghulam Mustafa")
 
 print(result)
